b0a92c847f0ec0e16822d04d6e4069a8df436515
upendra1611 <upendrayadav723>
2019-09-15 07:23:42 +0530

    first commit

---
+++ learning_templates/basic_app/urls.py
@@ -0,0 +1,10 @@
+from django.conf.urls import url
+from basic_app import views
+from django.urls import path,include
+
+app_name = 'basic_app'
+
+urlpatterns = [
+    path('relative/',views.relative,name='relative'),
+    path('other/',views.other,name='other'),
+]
